f82e02b24b3ee38ac0db4044f7ea0dacfbf1ba01
MarcosPaul0 <marcosphip7@gmail.com>
2025-08-05 21:07:43 -0300

    feat: implementa caso de uso para atualizar curso

---
+++ src/domain/application/use-cases/update-course/update-course.spec.ts
@@ -0,0 +1,110 @@
+import { AuthorizationService } from '@/infra/authorization/authorization.service';
+import { InMemoryCoursesRepository } from 'test/repositories/in-memory-courses-repository';
+import { InMemoryTeacherCoursesRepository } from 'test/repositories/in-memory-teacher-courses-repository';
+import { makeUser } from 'test/factories/make-user';
+import { makeCourse } from 'test/factories/make-course';
+import { CourseAlreadyExistsError } from '../errors/course-already-exists-error';
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error';
+import { UpdateCourseUseCase } from './update-course';
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
+
+let inMemoryCoursesRepository: InMemoryCoursesRepository;
+let inMemoryTeacherCoursesRepository: InMemoryTeacherCoursesRepository;
+let authorizationService: AuthorizationService;
+
+let sut: UpdateCourseUseCase;
+
+describe('Update Course', () => {
+  beforeEach(() => {
+    inMemoryCoursesRepository = new InMemoryCoursesRepository();
+    inMemoryTeacherCoursesRepository = new InMemoryTeacherCoursesRepository();
+    authorizationService = new AuthorizationService(
+      inMemoryTeacherCoursesRepository,
+    );
+
+    sut = new UpdateCourseUseCase(
+      inMemoryCoursesRepository,
+      authorizationService,
+    );
+  });
+
+  it('should be able to update a course', async () => {
+    const adminUser = makeUser({
+      name: 'John Doe',
+      email: 'johnDoe@fake.com',
+    });
+
+    const course = makeCourse({
+      name: 'Fake course',
+    });
+
+    inMemoryCoursesRepository.courses.push(course);
+
+    const result = await sut.execute({
+      courseId: course.id.toString(),
+      data: {
+        name: 'Fake course 2',
+      },
+      sessionUser: adminUser,
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(inMemoryCoursesRepository.courses[0].name).toEqual('Fake course 2');
+  });
+
+  it('should not be able to update a course if not exists', async () => {
+    const adminUser = makeUser({
+      name: 'John Doe',
+      email: 'johnDoe@fake.com',
+    });
+
+    const result = await sut.execute({
+      courseId: 'fake course id',
+      data: {
+        name: 'Fake course 2',
+      },
+      sessionUser: adminUser,
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).instanceOf(ResourceNotFoundError);
+  });
+
+  it('should not be able to update a course if user session is teacher', async () => {
+    const teacherUser = makeUser({
+      name: 'John Doe',
+      email: 'johnDoe@fake.com',
+      role: 'teacher',
+    });
+
+    const result = await sut.execute({
+      courseId: 'fake course id',
+      data: {
+        name: 'Fake course 2',
+      },
+      sessionUser: teacherUser,
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).instanceOf(NotAllowedError);
+  });
+
+  it('should not be able to update a course if user session is student', async () => {
+    const studentUser = makeUser({
+      name: 'John Doe',
+      email: 'johnDoe@fake.com',
+      role: 'student',
+    });
+
+    const result = await sut.execute({
+      courseId: 'fake course id',
+      data: {
+        name: 'Fake course 2',
+      },
+      sessionUser: studentUser,
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).instanceOf(NotAllowedError);
+  });
+});
---
+++ src/domain/application/use-cases/update-course/update-course.ts
@@ -0,0 +1,62 @@
+import { Either, left, right } from '@/core/either';
+import { Injectable } from '@nestjs/common';
+import { User } from '@/domain/entities/user';
+import { CoursesRepository } from '../../repositories/courses-repository';
+import { AuthorizationService } from '@/infra/authorization/authorization.service';
+import { Course } from '@/domain/entities/course';
+import { CourseAlreadyExistsError } from '../errors/course-already-exists-error';
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
+
+interface UpdateCourseData {
+  name?: string;
+}
+
+interface UpdateCourseUseCaseRequest {
+  courseId: string;
+  data: UpdateCourseData;
+  sessionUser: User;
+}
+
+type UpdateCourseUseCaseResponse = Either<
+  CourseAlreadyExistsError,
+  {
+    course: Course;
+  }
+>;
+
+@Injectable()
+export class UpdateCourseUseCase {
+  constructor(
+    private coursesRepository: CoursesRepository,
+    private authorizationService: AuthorizationService,
+  ) {}
+
+  async execute({
+    courseId,
+    data,
+    sessionUser,
+  }: UpdateCourseUseCaseRequest): Promise<UpdateCourseUseCaseResponse> {
+    const authorization = await this.authorizationService.ensureUserRole(
+      sessionUser,
+      ['admin'],
+    );
+
+    if (authorization.isLeft()) {
+      return left(authorization.value);
+    }
+
+    const course = await this.coursesRepository.findById(courseId);
+
+    if (!course) {
+      return left(new ResourceNotFoundError());
+    }
+
+    Object.assign(course, data);
+
+    await this.coursesRepository.save(course);
+
+    return right({
+      course,
+    });
+  }
+}
