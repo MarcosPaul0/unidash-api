04343c00c7e938987b6fbb132217dc333a04ffb7
MarcosPaul0 <marcosphip7@gmail.com>
2025-08-06 21:43:01 -0300

    feat: implementa rota para busca de docente por id

--- src/infra/http/controllers/find-teacher-by-id/find-teacher-by-id.controller.e2e-spec.ts
+++ src/infra/http/controllers/find-teacher-by-id/find-teacher-by-id.controller.e2e-spec.ts
@@ -7,27 +7,32 @@ import { DatabaseModule } from '../../../database/database.module';
 import { TeacherFactory } from 'test/factories/make-teacher';
 import { JwtService } from '@nestjs/jwt';
 import { USER_ROLE } from '@/domain/entities/user';
+import { AdminFactory } from 'test/factories/make-admin';
 
 describe('Find Teacher By Id (E2E)', () => {
   let app: INestApplication;
   let teacherFactory: TeacherFactory;
+  let adminFactory: AdminFactory;
   let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [TeacherFactory],
+      providers: [TeacherFactory, AdminFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
 
     jwt = moduleRef.get(JwtService);
     teacherFactory = moduleRef.get(TeacherFactory);
+    adminFactory = moduleRef.get(AdminFactory);
 
     await app.init();
   });
 
-  test('[GET] /teacher/me', async () => {
+  test('[GET] /teacher/:teacherId', async () => {
+    const admin = await adminFactory.makePrismaAdmin();
+
     const teacher = await teacherFactory.makePrismaTeacher({
       email: 'johndoe@example.com',
       password: await hash('123456', 8),
@@ -36,13 +41,13 @@ describe('Find Teacher By Id (E2E)', () => {
     });
 
     const accessToken = jwt.sign({
-      sub: teacher.id.toString(),
-      userRole: USER_ROLE.teacher,
+      sub: admin.id.toString(),
+      userRole: USER_ROLE.admin,
       accountActivatedAt: new Date(),
     });
 
     const response = await request(app.getHttpServer())
-      .get(`/teacher/me`)
+      .get(`/teacher/${teacher.id.toString()}`)
       .set('Authorization', `Bearer ${accessToken}`);
 
     expect(response.statusCode).toBe(200);
@@ -52,7 +57,7 @@ describe('Find Teacher By Id (E2E)', () => {
         name: teacher.name,
         email: teacher.email,
         role: teacher.role,
-        teacherRole: teacher.teacherRole,
+        isActive: teacher.isActive,
         accountActivatedAt: expect.any(String),
         createdAt: expect.any(String),
         updatedAt: expect.any(String),
--- src/infra/http/controllers/find-teacher-by-id/find-teacher-by-id.controller.ts
+++ src/infra/http/controllers/find-teacher-by-id/find-teacher-by-id.controller.ts
@@ -5,24 +5,28 @@ import {
   Get,
   HttpCode,
   NotFoundException,
+  Param,
 } from '@nestjs/common';
 import { FindTeacherByIdUseCase } from '@/domain/application/use-cases/find-teacher-by-id/find-teacher-by-id';
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
 import { CurrentUser } from '../../../auth/current-user-decorator';
-import { UserPayload } from '../../../auth/jwt.strategy';
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error';
 import { TeacherPresenter } from '../../presenters/teacher-presenter';
+import { User } from '@/domain/entities/user';
 
-@Controller('/teacher/me')
+@Controller('/teacher/:teacherId')
 export class FindTeacherByIdController {
   constructor(private findTeacherByIdUseCase: FindTeacherByIdUseCase) {}
 
   @Get()
   @HttpCode(200)
-  async handle(@CurrentUser() { sub, userRole }: UserPayload) {
+  async handle(
+    @CurrentUser() sessionUser: User,
+    @Param('teacherId') teacherId: string,
+  ) {
     const result = await this.findTeacherByIdUseCase.execute({
-      id: sub,
-      userRole,
+      teacherId,
+      sessionUser,
     });
 
     if (result.isLeft()) {
