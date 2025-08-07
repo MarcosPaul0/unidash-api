2caf60aa1b90cf5f550d1be19691bc64c6dfe639
MarcosPaul0 <marcosphip7@gmail.com>
2025-08-06 21:42:50 -0300

    feat: implementa rota para busca de docente

---
+++ src/infra/http/controllers/find-teacher/find-teacher.controller.e2e-spec.ts
@@ -0,0 +1,62 @@
+import { AppModule } from '@/infra/app.module';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+import { hash } from 'bcryptjs';
+import { DatabaseModule } from '../../../database/database.module';
+import { TeacherFactory } from 'test/factories/make-teacher';
+import { JwtService } from '@nestjs/jwt';
+import { USER_ROLE } from '@/domain/entities/user';
+
+describe('Find Teacher (E2E)', () => {
+  let app: INestApplication;
+  let teacherFactory: TeacherFactory;
+  let jwt: JwtService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [TeacherFactory],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    jwt = moduleRef.get(JwtService);
+    teacherFactory = moduleRef.get(TeacherFactory);
+
+    await app.init();
+  });
+
+  test('[GET] /teacher/me', async () => {
+    const teacher = await teacherFactory.makePrismaTeacher({
+      email: 'johndoe@example.com',
+      password: await hash('123456', 8),
+      accountActivatedAt: new Date(),
+      role: USER_ROLE.teacher,
+    });
+
+    const accessToken = jwt.sign({
+      sub: teacher.id.toString(),
+      userRole: USER_ROLE.teacher,
+      accountActivatedAt: new Date(),
+    });
+
+    const response = await request(app.getHttpServer())
+      .get(`/teacher/me`)
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      teacher: expect.objectContaining({
+        id: teacher.id.toString(),
+        name: teacher.name,
+        email: teacher.email,
+        role: teacher.role,
+        isActive: teacher.isActive,
+        accountActivatedAt: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+      }),
+    });
+  });
+});
---
+++ src/infra/http/controllers/find-teacher/find-teacher.controller.ts
@@ -0,0 +1,44 @@
+import {
+  BadRequestException,
+  Controller,
+  ForbiddenException,
+  Get,
+  HttpCode,
+  NotFoundException,
+} from '@nestjs/common';
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
+import { CurrentUser } from '../../../auth/current-user-decorator';
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error';
+import { TeacherPresenter } from '../../presenters/teacher-presenter';
+import { FindTeacherUseCase } from '@/domain/application/use-cases/find-teacher/find-teacher';
+import { User } from '@/domain/entities/user';
+
+@Controller('/teacher/me')
+export class FindTeacherController {
+  constructor(private findTeacherUseCase: FindTeacherUseCase) {}
+
+  @Get()
+  @HttpCode(200)
+  async handle(@CurrentUser() sessionUser: User) {
+    const result = await this.findTeacherUseCase.execute({
+      sessionUser,
+    });
+
+    if (result.isLeft()) {
+      const error = result.value;
+
+      switch (error.constructor) {
+        case ResourceNotFoundError:
+          throw new NotFoundException(error.message);
+        case NotAllowedError:
+          throw new ForbiddenException(error.message);
+        default:
+          throw new BadRequestException(error.message);
+      }
+    }
+
+    return {
+      teacher: TeacherPresenter.toHTTP(result.value.teacher),
+    };
+  }
+}
