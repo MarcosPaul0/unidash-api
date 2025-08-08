f5d03e191da1da8f7cb84e4a8e4acce8b134594e
MarcosPaul0 <marcosphip7@gmail.com>
2025-08-07 22:47:54 -0300

    refactor: atualiza busca de discente para docente e admin

---
+++ src/domain/application/use-cases/find-all-students/find-all-students.ts
@@ -0,0 +1,48 @@
+import { Either, left, right } from '@/core/either';
+import { Injectable } from '@nestjs/common';
+import { StudentsRepository } from '../../repositories/students-repository';
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error';
+import { Pagination } from '@/core/pagination/pagination';
+import { Student } from '@/domain/entities/student';
+import { User } from '@/domain/entities/user';
+import { AuthorizationService } from '@/infra/authorization/authorization.service';
+
+interface FindAllStudentsUseCaseRequest {
+  sessionUser: User;
+  pagination?: Pagination;
+}
+
+type FindAllStudentsUseCaseResponse = Either<
+  NotAllowedError,
+  {
+    students: Student[];
+    totalItems: number;
+    totalPages: number;
+  }
+>;
+
+@Injectable()
+export class FindAllStudentsUseCase {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private authorizationService: AuthorizationService,
+  ) {}
+
+  async execute({
+    pagination,
+    sessionUser,
+  }: FindAllStudentsUseCaseRequest): Promise<FindAllStudentsUseCaseResponse> {
+    const authorization = await this.authorizationService.ensureUserRole(
+      sessionUser,
+      ['admin', 'teacher'],
+    );
+
+    if (authorization.isLeft()) {
+      return left(authorization.value);
+    }
+
+    const students = await this.studentsRepository.findAll(pagination);
+
+    return right(students);
+  }
+}
