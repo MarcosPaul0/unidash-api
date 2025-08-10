c7b7b13fc564b46d86b905157eb119e782225b49
MarcosPaul0 <marcosphip7@gmail.com>
2025-08-10 20:40:49 -0300

    feat: implementa caso de uso para busca de dado de saída de alunos

---
+++ src/domain/application/use-cases/find-all-course-departure-data/find-all-course-departure-data.spec.ts
@@ -0,0 +1,76 @@
+import { InMemoryCoursesRepository } from 'test/repositories/in-memory-courses-repository';
+import { makeCourse } from 'test/factories/make-course';
+import { FindAllCourseDepartureDataUseCase } from './find-all-course-departure-data';
+import { InMemoryCourseDepartureDataRepository } from 'test/repositories/in-memory-course-departure-data-repository';
+import { makeCourseDepartureData } from 'test/factories/make-course-departure-data';
+
+let inMemoryCourseDepartureDataRepository: InMemoryCourseDepartureDataRepository;
+
+let sut: FindAllCourseDepartureDataUseCase;
+
+describe('Find All Course Departure Data', () => {
+  beforeEach(() => {
+    inMemoryCourseDepartureDataRepository =
+      new InMemoryCourseDepartureDataRepository();
+    sut = new FindAllCourseDepartureDataUseCase(
+      inMemoryCourseDepartureDataRepository,
+    );
+  });
+
+  it('should be able to find all course departure data', async () => {
+    const courseDepartureData1 = makeCourseDepartureData();
+    const courseDepartureData2 = makeCourseDepartureData();
+    const courseDepartureData3 = makeCourseDepartureData();
+
+    inMemoryCourseDepartureDataRepository.courseDepartureData.push(
+      courseDepartureData1,
+      courseDepartureData2,
+      courseDepartureData3,
+    );
+
+    const result = await sut.execute({
+      pagination: { page: 1, itemsPerPage: 2 },
+      filters: { semester: 'first', year: 2025 },
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toEqual({
+      courseDepartureData: [courseDepartureData1, courseDepartureData2],
+      totalItems: 3,
+      totalPages: 2,
+    });
+  });
+
+  it('should be able to find all course departure data with filters', async () => {
+    const courseDepartureData1 = makeCourseDepartureData({
+      semester: 'first',
+      year: 2025,
+    });
+    const courseDepartureData2 = makeCourseDepartureData({
+      semester: 'second',
+      year: 2025,
+    });
+    const courseDepartureData3 = makeCourseDepartureData({
+      semester: 'second',
+      year: 2024,
+    });
+
+    inMemoryCourseDepartureDataRepository.courseDepartureData.push(
+      courseDepartureData1,
+      courseDepartureData2,
+      courseDepartureData3,
+    );
+
+    const result = await sut.execute({
+      pagination: { page: 1, itemsPerPage: 2 },
+      filters: { semester: 'second', year: 2024 },
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toEqual({
+      courseDepartureData: [courseDepartureData3],
+      totalItems: 1,
+      totalPages: 1,
+    });
+  });
+});
---
+++ src/domain/application/use-cases/find-all-course-departure-data/find-all-course-departure-data.ts
@@ -0,0 +1,40 @@
+import { Either, right } from '@/core/either';
+import { Injectable } from '@nestjs/common';
+import { Pagination } from '@/core/pagination/pagination';
+import {
+  CourseDepartureDataRepository,
+  FindAllCourseDepartureDataFilter,
+} from '../../repositories/course-departure-data-repository';
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
+import { CourseDepartureData } from '@/domain/entities/course-departure-data';
+
+interface FindAllCourseDepartureDataUseCaseRequest {
+  pagination?: Pagination;
+  filters?: FindAllCourseDepartureDataFilter;
+}
+
+type FindAllCourseDepartureDataUseCaseResponse = Either<
+  ResourceNotFoundError,
+  {
+    courseDepartureData: CourseDepartureData[];
+    totalItems: number;
+    totalPages: number;
+  }
+>;
+
+@Injectable()
+export class FindAllCourseDepartureDataUseCase {
+  constructor(
+    private courseDepartureDataRepository: CourseDepartureDataRepository,
+  ) {}
+
+  async execute({
+    pagination,
+    filters,
+  }: FindAllCourseDepartureDataUseCaseRequest): Promise<FindAllCourseDepartureDataUseCaseResponse> {
+    const courseDepartureData =
+      await this.courseDepartureDataRepository.findAll(pagination, filters);
+
+    return right(courseDepartureData);
+  }
+}
