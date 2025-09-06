b65d0bdc9edc29fe1feda7583e435d83d6692b9d
MarcosPaul0 <marcosphip7@gmail.com>
2025-09-05 22:04:14 -0300

    feat: adiciona novas migrations

---
+++ prisma/migrations/20250831011141_alter_unique_student_constraint/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "students_courseId_key";
---
+++ prisma/migrations/20250904222905_add_vacancies_and_subscribers_column_to_course_students_table/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "courseStudentsData" ADD COLUMN     "subscribers" SMALLINT NOT NULL DEFAULT 0,
+ADD COLUMN     "vacancies" SMALLINT NOT NULL DEFAULT 0;
