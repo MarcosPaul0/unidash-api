6a985df3c79fc5e544634a667a53f752362b4cfc
MarcosPaul0 <marcosphip7@gmail.com>
2025-09-06 09:04:17 -0300

    feat: implementa validação de permissão para deleção de docente

--- src/domain/application/use-cases/delete-teacher/delete-teacher.spec.ts
+++ src/domain/application/use-cases/delete-teacher/delete-teacher.spec.ts
@@ -3,33 +3,51 @@ import { InMemoryTeachersRepository } from 'test/repositories/in-memory-teachers
 import { makeTeacher } from 'test/factories/make-teacher';
 import { UniqueEntityId } from '@/core/entities/unique-entity-id';
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
+import { AuthorizationService } from '@/infra/authorization/authorization.service';
+import { InMemoryTeacherCoursesRepository } from 'test/repositories/in-memory-teacher-courses-repository';
+import { makeAdmin } from 'test/factories/make-admin';
+import { makeSessionUser } from 'test/factories/make-session-user';
 
 let inMemoryTeachersRepository: InMemoryTeachersRepository;
+let inMemoryTeacherCoursesRepository: InMemoryTeacherCoursesRepository;
+let authorizationService: AuthorizationService;
 
 let sut: DeleteTeacherUseCase;
 
 describe('Delete Teacher', () => {
   beforeEach(() => {
     inMemoryTeachersRepository = new InMemoryTeachersRepository();
+    inMemoryTeacherCoursesRepository = new InMemoryTeacherCoursesRepository();
+    authorizationService = new AuthorizationService(
+      inMemoryTeacherCoursesRepository,
+    );
 
-    sut = new DeleteTeacherUseCase(inMemoryTeachersRepository);
+    sut = new DeleteTeacherUseCase(
+      inMemoryTeachersRepository,
+      authorizationService,
+    );
   });
 
   it('should be able to delete a teacher', async () => {
+    const admin = makeAdmin();
     const newTeacher = makeTeacher({}, new UniqueEntityId('teacher-1'));
 
     inMemoryTeachersRepository.create(newTeacher);
 
     await sut.execute({
       teacherId: 'teacher-1',
+      sessionUser: makeSessionUser(admin),
     });
 
     expect(inMemoryTeachersRepository.teachers).toHaveLength(0);
   });
 
   it('should not be possible to delete a teacher that does not exist', async () => {
+    const admin = makeAdmin();
+
     const result = await sut.execute({
       teacherId: 'teacher-2',
+      sessionUser: makeSessionUser(admin),
     });
 
     expect(result.isLeft()).toBe(true);
--- src/domain/application/use-cases/delete-teacher/delete-teacher.ts
+++ src/domain/application/use-cases/delete-teacher/delete-teacher.ts
@@ -2,9 +2,12 @@ import { Either, left, right } from '@/core/either';
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
 import { Injectable } from '@nestjs/common';
 import { TeachersRepository } from '../../repositories/teacher-repository';
+import { AuthorizationService } from '@/infra/authorization/authorization.service';
+import { SessionUser } from '@/domain/entities/user';
 
 interface DeleteTeacherUseCaseRequest {
   teacherId: string;
+  sessionUser: SessionUser;
 }
 
 type DeleteTeacherUseCaseResponse = Either<
@@ -14,11 +17,24 @@ type DeleteTeacherUseCaseResponse = Either<
 
 @Injectable()
 export class DeleteTeacherUseCase {
-  constructor(private teachersRepository: TeachersRepository) {}
+  constructor(
+    private teachersRepository: TeachersRepository,
+    private authorizationService: AuthorizationService,
+  ) {}
 
   async execute({
     teacherId,
+    sessionUser,
   }: DeleteTeacherUseCaseRequest): Promise<DeleteTeacherUseCaseResponse> {
+    const authorization = await this.authorizationService.ensureUserRole(
+      sessionUser,
+      ['admin'],
+    );
+
+    if (authorization.isLeft()) {
+      return left(authorization.value);
+    }
+
     const teacher = await this.teachersRepository.findById(teacherId);
 
     if (!teacher) {
